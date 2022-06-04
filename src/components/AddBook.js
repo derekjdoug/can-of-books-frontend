a7419618c4fc99a5b7abb77638cc7092039bfdc0
Aiden <hyena.shenanigans@gmail.com>
2022-06-04 16:47:53 -0700

    Organized components, refactored carousel, build DB creation component, added TODOs

---
+++ src/components/AddBook.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+// DONE: pass showModal, handleBookCreate(), hideModal, and modal var in state above
+// TODO: change backend to include boolean status
+class AddBook extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleBookCreate({ title: event.target.formTitle.value, description: event.target.formDescription.value, status: event.target.formStatus.value });
+  }
+  render() {
+    return (
+      <Modal show={this.props.modalState} onHide={this.props.hideModal}>
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a new book</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group className="mb-3" controlId="formTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="name" placeholder="Book title go here" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="name" placeholder="Description" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formStatus">
+                <Form.Label>Status</Form.Label>
+                <Form.Control type="checkbox" label="Available?" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.hideModal} variant="secondary">
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
+    )
+  }
+}
+
+export default AddBook;
